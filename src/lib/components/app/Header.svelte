15a6423e286e3b69faccd1bb5b1023a70956ad0a
f-elix <guerinfelix08@gmail.com>
2025-04-09 15:11:51 -0400

    Add header

---
+++ src/lib/components/app/Header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import ThemeToggle from '$lib/components/app/ThemeToggle.svelte';
+</script>
+
+<header class="flex items-center justify-between bg-sidebar p-4 text-sidebar-foreground">
+	<h1>Powerkit</h1>
+	<ThemeToggle />
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+	import Header from '$lib/components/app/Header.svelte';
+	import Head from '$lib/components/app/Head.svelte';
 
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
+<Head />
+<Header />
 {@render children()}
