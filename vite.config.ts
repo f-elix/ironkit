e9c80de935eb97051786024c8f137c59f9fe2dcb
f-elix <guerinfelix08@gmail.com>
2025-04-09 15:12:41 -0400

    Add plugin kit routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { kitRoutes } from 'vite-plugin-kit-routes';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), kitRoutes({ format: 'variables' })],
 	test: {
 		workspace: [
 			{
