b12488910b9fb1d90e8aba181ef28bb17e231b9a
f-elix <guerinfelix08@gmail.com>
2025-04-20 16:27:50 -0400

    REmove unused file

--- src/lib/server/kv.ts
+++
@@ -1,7 +0,0 @@
-import { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } from '$env/static/private';
-import { Redis } from '@upstash/redis';
-
-export const kv = new Redis({
-	url: UPSTASH_REDIS_REST_URL,
-	token: UPSTASH_REDIS_REST_TOKEN
-});
