58800013935efd57161877a91da414549b10ac5f
f-elix <guerinfelix08@gmail.com>
2025-04-23 16:34:03 -0400

    Add finish workout button

--- src/routes/tools/training-log/workout-[id]/+page@.svelte
+++ src/routes/tools/training-log/workout-[id]/+page@.svelte
@@ -3,7 +3,10 @@
 	import PerformanceGroups from '$lib/components/training-log/PerformanceGroups.svelte';
 	import WorkoutHeader from '$lib/components/training-log/WorkoutHeader.svelte';
 	import { triplit } from '$lib/db/triplit';
+	import Button from '$lib/shadcn/button/button.svelte';
 	import { useQuery } from '@triplit/svelte';
+	import Check from '@lucide/svelte/icons/check';
+	import { PAGE_tools_training_log } from '$lib/ROUTES';
 
 	const workoutId = page.params.id;
 	const query = useQuery(triplit, triplit.query('workouts').Where('id', '=', workoutId));
@@ -12,8 +15,20 @@
 </script>
 
 {#if workout}
-	<div class="grow p-4">
-		<WorkoutHeader {workout} />
-		<PerformanceGroups {workoutId} />
+	<div class="flex grow flex-col">
+		<div class="p-4">
+			<WorkoutHeader {workout} />
+			<PerformanceGroups {workoutId} />
+		</div>
+		<div class="mt-auto flex w-full flex-col p-4">
+			<Button
+				href={PAGE_tools_training_log}
+				size="lg"
+				class="bg-emerald-500 text-lg font-semibold [&_svg]:size-5"
+			>
+				Finish workout
+				<Check />
+			</Button>
+		</div>
 	</div>
 {/if}
