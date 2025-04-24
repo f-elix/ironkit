60976d9abc446f90723bd9a60455fd04f9eb917d
f-elix <guerinfelix08@gmail.com>
2025-04-24 13:06:55 -0400

    Remove api exclusion

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -34,10 +34,6 @@ self.addEventListener('fetch', (event) => {
 		return;
 	}
 
-	if (event.request.url.includes('/api/sync')) {
-		return;
-	}
-
 	const respond = async () => {
 		const url = new URL(event.request.url);
 		const cache = await caches.open(CACHE);
