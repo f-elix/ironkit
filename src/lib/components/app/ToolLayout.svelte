b9facea139a85d5d1a7e8d5fc62b636d881084d7
f-elix <guerinfelix08@gmail.com>
2025-05-21 15:18:14 -0400

    Adjust tool layout for dialogs

--- src/lib/components/app/ToolLayout.svelte
+++ src/lib/components/app/ToolLayout.svelte
@@ -16,10 +16,8 @@
 	};
 </script>
 
-<div class="flex grow flex-col">
-	<output for={outputIdList} class="px-4 pb-2">
-		{@render output?.()}
-	</output>
+<!-- Use flex-col-reverse to preserve the layout while making sure the form elements receive focused first when in a dialog -->
+<div class="flex grow flex-col-reverse">
 	<form use:setup class="relative mt-auto flex flex-col items-center gap-8" novalidate>
 		{@render input?.()}
 		<div
@@ -31,4 +29,7 @@
 			{@render settings?.()}
 		</div>
 	</form>
+	<output for={outputIdList} class="px-4 pb-2">
+		{@render output?.()}
+	</output>
 </div>
