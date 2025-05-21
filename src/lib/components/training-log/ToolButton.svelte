06f3ce0fb0dfdcc673030f2599263e5906ab7e3c
f-elix <guerinfelix08@gmail.com>
2025-05-21 15:16:54 -0400

    Show tools inside a sheet on desktop

---
+++ src/lib/components/training-log/ToolButton.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { pushState } from '$app/navigation';
+
+	import type { Tool } from '$lib/data/tools';
+	import { isDesktop } from '$lib/isDesktop.svelte';
+	import { PAGE_tools_training_log } from '$lib/ROUTES';
+	import Button from '$lib/shadcn/button/button.svelte';
+
+	let { href, title, Icon }: Tool = $props();
+
+	const onclick = (e: Event) => {
+		if (!isDesktop.current || href === PAGE_tools_training_log) {
+			return;
+		}
+		e.preventDefault();
+		pushState('', {
+			toolHref: href
+		});
+	};
+</script>
+
+<Button
+	variant="secondary"
+	size="lg"
+	class="aspect-[4/3] h-auto w-full flex-col items-center border bg-gray-900 px-2 text-center whitespace-normal lg:text-xl"
+	{href}
+	{onclick}
+>
+	<Icon class="lg:size-8" />
+	<span>{title}</span>
+</Button>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Logo from '$lib/components/svg/Logo.svelte';
-	import Button from '$lib/shadcn/button/button.svelte';
 	import { tools } from '$lib/data/tools';
+	import ToolButton from '$lib/components/training-log/ToolButton.svelte';
+	import { page } from '$app/state';
+	import ToolDialog from '$lib/components/training-log/ToolDialog.svelte';
 </script>
 
 <section class="flex flex-col items-center gap-1 p-4 pt-10 text-center">
@@ -16,18 +18,12 @@
 	<ul
 		class="mx-auto grid max-w-100 grid-cols-2 gap-2 md:max-w-200 md:grid-cols-3 md:gap-4 lg:max-w-240"
 	>
-		{#each Object.values(tools) as { Icon, title, href }}
+		{#each Object.values(tools) as tool}
 			<li>
-				<Button
-					variant="secondary"
-					size="lg"
-					class="aspect-[4/3] h-auto w-full flex-col items-center border bg-gray-900 px-2 text-center whitespace-normal lg:text-xl"
-					{href}
-				>
-					<Icon class="lg:size-8" />
-					<span>{title}</span>
-				</Button>
+				<ToolButton {...tool} />
 			</li>
 		{/each}
 	</ul>
 </section>
+
+<ToolDialog toolHref={page.state.toolHref} />
