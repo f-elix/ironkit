a0f5971d417756197c3bb269a5c3066df2019bdc
f-elix <guerinfelix08@gmail.com>
2026-01-31 22:36:24 -0500

    better virtual keyboard handling

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,21 @@
 declare global {
 	type Maybe<T> = T | null | undefined;
+
+	/**
+	 * VirtualKeyboard API types (Chromium 94+)
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/VirtualKeyboard_API
+	 */
+	interface VirtualKeyboard extends EventTarget {
+		readonly boundingRect: DOMRect;
+		overlaysContent: boolean;
+		hide(): void;
+		show(): void;
+		ongeometrychange: ((this: VirtualKeyboard, ev: Event) => unknown) | null;
+	}
+
+	interface Navigator {
+		readonly virtualKeyboard?: VirtualKeyboard;
+	}
 }
 
 export {};
